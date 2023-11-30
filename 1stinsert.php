774fae5ac02f0290d7d91b00b2a2fcb9679fdde7
ananthu092002 <145977411+ananthu092002@users.noreply.github.com>
2023-11-30 14:25:57 +0530

    Add files via upload

---
+++ 1stinsert.php
@@ -0,0 +1,41 @@
+<html>
+<body bgcolor="yellow">
+<?php
+require('1stlogin.php');
+if(isset($_POST['submitt']))
+{
+
+$fname=$_POST['fname'];
+$lname=$_POST['lname'];
+$pass=$_POST['pass'];
+$sql="insert into login(firstname,lastname,password)values('$fname','$lname','$pass')";
+$result=mysqli_query($conn,$sql);
+if($result)
+
+echo"new records successfully created";
+
+
+mysqli_close($conn);
+}
+else
+{
+?>
+<form action=""method="POST">
+<center>
+<label for="Name">Enter first Name:</label>
+<input type="text"name="fname" id="Name" placeholder="first Name"><br>
+<label for="Name">Enter last Name:</label>
+<input type="text"name="lname" id="Name"placeholder="last Name"><br>
+<label>Password  :
+<input type="password"name="pass"placeholder="enter password Here"><br></label>
+<br>
+<br>
+<input type="submit"name="submitt"value="create now">
+<br>
+
+<?php
+}
+?>
+</form>
+</body>
+</html>
